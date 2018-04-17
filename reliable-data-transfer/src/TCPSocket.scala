e3adf99eb39eb9c84379ddaea05d963a5ffce396
Abdelrahman Abdelnabi <abdoumohamed96@gmail.com>
2018-04-17 17:54:50 +0200

    just updating file modes (ignore commit)

--- reliable-data-transfer/src/TCPSocket.scala
+++ reliable-data-transfer/src/TCPSocket.scala
@@ -1,3 +1,17 @@
-class TCPSocket {
+import java.net.{DatagramPacket, DatagramSocket, InetAddress}
+
+class TCPSocket(val address: InetAddress, val port: Int) {
+
+  val UDPSocket: DatagramSocket = new DatagramSocket()
+
+  // corresponds to a call from above
+  def send(message: String) {
+
+    val data: Array[Byte] = message.getBytes
+    val packet = new DatagramPacket(data, data.length, address, 4445)
+
+    UDPSocket.send(packet)
+
+  }
 
 }
