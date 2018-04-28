7b5a46477f6bb53940af2cb4626f20cbcc935fb9
Abdelrahman Abdelnabi <abdoumohamed96@gmail.com>
2018-04-28 21:24:41 +0200

    Update timer implementation to support any range of sequence numbers

--- reliable-data-transfer/src/sender/MyTimer.scala
+++ reliable-data-transfer/src/sender/MyTimer.scala
@@ -1,35 +1,25 @@
 package sender
 
+import java.util
 import java.util.TimerTask
 import java.util.Timer
 
-import sender.StopAndWaitSender.StopAndWaitSender
 
 class MyTimer(listener: Sender, timeOut: Int) {
-  private var timerZero: Timer = _
-  private var timerOne: Timer = _
+  private val timers: util.HashMap[Int, Timer] = new util.HashMap()
 
   def start(seqNo: Int): Unit = {
-    seqNo match {
-      case 0 => timerZero = new Timer()
-        timerZero.schedule(new TimerTask {
-          override def run(): Unit = {
-            listener.timeout(0)
-          }
-        }, timeOut)
-      case 1 => timerOne = new Timer()
-        timerOne.schedule(new TimerTask {
-          override def run(): Unit = {
-            listener.timeout(1)
-          }
-        }, timeOut)
+    timers.put(seqNo, new Timer())
+    timers.get(seqNo).schedule(new MyTask(seqNo), timeOut)
+  }
+
+  class MyTask(seqNo: Int) extends TimerTask {
+    override def run(): Unit = {
+      listener.timeout(seqNo)
     }
   }
 
   def cancel(seqNo: Int): Unit = {
-    seqNo match {
-      case 0 => timerZero.cancel()
-      case 1 => timerOne.cancel()
-    }
+     timers.get(seqNo).cancel()
   }
 }
